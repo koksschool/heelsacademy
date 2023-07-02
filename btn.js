a23156b0d4b1c3bc79d8f62956f430f2ad9e6f3a
koksschool <136823143+koksschool@users.noreply.github.com>
2023-07-02 16:26:32 +0300

    Update btn.js

--- btn.js
+++ btn.js
@@ -1,4 +1,4 @@
-var images = ["Pics/1.jpg", "Pics/2.jpg", "Pics/3.jpg", "Pics/4.jpg", "Pics/5.jpg", "Pics/6.jpg", "Pics/7.jpg", "Pics/8.jpg", "Pics/9.jpg", "Pics/10.jpg", "Pics/11.jpg", "Pics/12.jpg", "Pics/13.jpg", "Pics/14.jpg",
+var images = ["Pics/1.JPG", "Pics/2.JPG", "Pics/3.JPG", "Pics/4.JPG", "Pics/5.JPG", "Pics/6.JPG", "Pics/7.JPG", "Pics/8.JPG", "Pics/9.JPG", "Pics/10.JPG", "Pics/11.JPG", "Pics/12.JPG", "Pics/13.JPG", "Pics/14.JPG",
 ];
 var currentIndex = 0;
 
